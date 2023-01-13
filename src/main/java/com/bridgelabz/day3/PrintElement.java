a1f0f37547a1a559f887bf4b9e1a5f09a4f9752f
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-13 16:47:53 +0530

    Added: Print all elements in an array

---
+++ src/main/java/com/bridgelabz/day3/PrintElement.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day3;
+
+public class PrintElement {
+    public static void main(String[] args) {
+        int[] Array1 = {3, 5, 42, 94, 23, 13};
+        System.out.print("Elements in Array are: ");
+        for (int i=0; i< Array1.length; i++) {
+            System.out.print(Array1[i] + " ");
+        }
+    }
+}
