46263dc8edc5d9018a1fe7867e59e00887776888
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-13 17:00:53 +0530

    Added: Printing elements in odd position

---
+++ src/main/java/com/bridgelabz/day3/OddPositionElement.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day3;
+
+public class OddPositionElement {
+    public static void main(String[] args) {
+        int[] Array1 = {3, 5, 42, 94, 23, 13};
+        for (int i = 0; i<Array1.length; i++) {
+            if(i%2!=0)
+                System.out.print(Array1[i] + " ");
+        }
+    }
+}
